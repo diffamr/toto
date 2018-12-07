c58abdb12230ace478b96d62c222ba40583d66c5
diffamr <44928835+diffamr@users.noreply.github.com>
2018-12-07 17:12:41 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh4Pbu9N-o7-1d869fX5sf3m3z4-5gafAnnWrhKDJloCKi5e7qoqktNuLz62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "49dd3c2eb8d1dee9";
+$os="android";
+$sign="9a309ade6760911a0b929f47fc250480a916fe0085da83f4432cb454ab4afd0e";
